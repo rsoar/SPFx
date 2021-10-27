0a2dbc84d7c73e99ad0ad09a4a1dc8c1db280f7d
rsoar <r.soaresmedeiros@hotmail.com>
2021-10-27 16:53:39 -0300

    feat: peoplePicker/add: setAction

--- src/webparts/cobranca/components/Modal/Add/Add.tsx
+++ src/webparts/cobranca/components/Modal/Add/Add.tsx
@@ -4,22 +4,30 @@ import { IDataClient } from '../../../Interface/IDataClient';
 
 import styles from '../Modal.module.scss';
 
+import { IPersonaProps } from 'office-ui-fabric-react';
+
 interface IProps {
   client: IDataClient
   handleModal: (dataClient) => void;
   defineValueInput: (e: any) => void;
   addClient: () => void;
+  updateClient: (dataClient) => void;
+  action: number;
+  picker: any;
+  peopleSelected: IPersonaProps;
+  pickerMethod: () => void;
 }
 
-export const Add = ({ client, handleModal, defineValueInput, addClient }: IProps) => {
+export const Add = ({pickerMethod, peopleSelected, client, handleModal, defineValueInput, addClient, updateClient, action, picker}: IProps) => {
   
   return(
     <div className={styles.modalBackground}>
       <div className={styles.modalContent}>
         <button className={styles.closeModal} onClick={handleModal}>X</button>
-        <h1>ADICIONAR NOVO CLIENTE</h1>
+        { action !== 0 ? <h1>Editar cliente</h1> : <h1>Adicionar novo cliente</h1> }
         <label>Nome do cliente:</label>
-        <input className={styles.inpt} name="Title" type="text" placeholder="Digite o nome completo do cliente" value={client.Title} onChange={defineValueInput} />
+        {pickerMethod()}
+        {/* <input className={styles.inpt} name="Title" type="text" placeholder="Digite o nome completo do cliente" value={client.Title} onChange={defineValueInput} /> */}
         <label>Motivo:</label>
         <input className={styles.inpt} name="Motivo" type="text" placeholder="Motivo do atendimento" value={client.Motivo} onChange={defineValueInput} />
         <label>Situação:</label>
@@ -28,7 +36,7 @@ export const Add = ({ client, handleModal, defineValueInput, addClient }: IProps
           <option value="Pendente">Em aberto</option>
           <option value="Finalizado">Finalizado</option>
         </select>
-        <button className={styles.addButton} onClick={addClient}>Adicionar</button>
+        {action !== 0 ? <button className={styles.addButton} onClick={() => updateClient(client)}>ATUALIZAR</button> : <button className={styles.addButton} onClick={addClient}>ADICIONAR</button> }
       </div>
     </div>
   )
